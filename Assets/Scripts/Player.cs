44789331543dc466de1f64a33e96d8fd4c5c0243
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-30 23:44:35 +0600

    jack the giant

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
 
-public class Player : MonoBehaviour 
-{
+public class Player:MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -14,4 +13,6 @@ public class Player : MonoBehaviour
 	void Update () {
 
 	}
+
+
 }
