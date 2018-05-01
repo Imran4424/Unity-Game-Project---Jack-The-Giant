4190ac45d7dc800b6433ec3b52cf148523db7e06
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-01 06:40:45 +0600

    jack the giant

--- Assets/Scripts/BackGroundScript.cs
+++ Assets/Scripts/BackGroundScript.cs
@@ -12,7 +12,7 @@ public class BackGroundScript : MonoBehaviour {
 
 		float width = sr.sprite.bounds.size.x;
 
-		float worldHeight = Camera.main.orthographicSize * 2;
+		float worldHeight = Camera.main.orthographicSize * 2.0f;
 	}
 	
 }
