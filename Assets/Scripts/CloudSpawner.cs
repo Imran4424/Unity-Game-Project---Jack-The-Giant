35066cf958c1786725b200b2409d2f31df8fe27c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-02 20:45:19 +0600

    jack the jiant

--- Assets/Scripts/CloudSpawner.cs
+++ Assets/Scripts/CloudSpawner.cs
@@ -99,6 +99,6 @@ public class CloudSpawner : MonoBehaviour
 
 	void positionThePlayer()
 	{
-		GameObject[] darkClouds = GameObject.FindGameObjectsWithTag("Dead");
+		GameObject[] darkClouds = GameObject.FindGameObjectsWithTag("Deadly");
 	}
 }
