c4c8f90fb8e80908f3671acdf6ab778bb49b1837
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-02 21:00:52 +0600

    jack the jiant

--- Assets/Scripts/CloudSpawner.cs
+++ Assets/Scripts/CloudSpawner.cs
@@ -106,7 +106,7 @@ public class CloudSpawner : MonoBehaviour
 		{
 			if (darkClouds[i].transform.position.y == 0)
 			{
-				Vector3 tempDarkCloud 				
+				Vector3 tempDarkCloud = 				
 			}	
 		}
 	}
