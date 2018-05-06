fb957627e8c6b2f8c9be26b12ce9ac2034556317
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-06 23:12:22 +0600

    jack the Giant

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -6,7 +6,7 @@ public class CameraScript : MonoBehaviour {
 
 
 	private float speed = 1f;
-	private float acceleration = 
+	private float acceleration = 0.2f;
 
 	// Use this for initialization
 	void Start () 
