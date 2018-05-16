7b24e823824f600b1b8267a72e032a95b66fd9b4
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-16 23:31:36 +0600

    jack the Giant

--- Assets/Scripts/Game Controllers/MainMenuController.cs
+++ Assets/Scripts/Game Controllers/MainMenuController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scene
 
 public class MainMenuController : MonoBehaviour {
 
@@ -12,7 +13,7 @@ public class MainMenuController : MonoBehaviour {
 	
 	public void StartGame()
 	{
-		
+		Scene
 	}
 	
 }
