594059a2e881319f1cc720abfba14eb2018f3ee2
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-25 04:51:02 +0600

    jack the giant - game preferences

--- Assets/Scripts/Game Preferences/GamePreferences.cs
+++ Assets/Scripts/Game Preferences/GamePreferences.cs
@@ -8,4 +8,16 @@ public static class GamePreferences
 	public static string EasyDifficulty = "EasyDifficulty";
 	public static string MediumDifficulty = "EasyDifficulty";
 	public static string HardDifficulty = "EasyDifficulty";
+
+
+	public static string EasyDifficultyScore = "EasyDifficulty";
+	public static string MediumDifficultyScore = "EasyDifficulty";
+	public static string HardDifficultyScore = "EasyDifficulty";
+
+
+
+	public static string EasyDifficultyCoinScore = "EasyDifficulty";
+	public static string MediumDifficultyCoinScore = "EasyDifficulty";
+	public static string HardDifficultyCoinScore = "EasyDifficulty";
+
 }
