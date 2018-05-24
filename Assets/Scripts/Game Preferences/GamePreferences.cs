a7dce5ad3277aa17e20ab9b60a2fb159c3b09187
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-25 04:30:07 +0600

    jack the giant - game preferences

--- Assets/Scripts/Game Preferences/GamePreferences.cs
+++ Assets/Scripts/Game Preferences/GamePreferences.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class GamePreferences{
+public static class GamePreferences
+{
 
-	
+	public static string EasyDifficulty = "EasyDifficulty";
 }
