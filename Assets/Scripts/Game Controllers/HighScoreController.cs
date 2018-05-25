f2ad4c3ed12dbd209f029d966a7718856217aa34
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-25 12:31:32 +0600

    jack the giant - game preferences

--- Assets/Scripts/Game Controllers/HighScoreController.cs
+++ Assets/Scripts/Game Controllers/HighScoreController.cs
@@ -4,31 +4,46 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class HighScoreController : MonoBehaviour {
+public class HighScoreController : MonoBehaviour
+{
 
 	[SerializeField]
 	private Text scoreText, coinText;
 
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
-		
-	}
-	
-	void SetScore(int score, int coins)
-	{
-		scoreText.text = score.ToString();
-		coinText.text = coins.ToString();
+
 	}
 
-	void SetScoreBasedOnDifficulty()
+	void SetScore (int score, int coins)
 	{
-		
+		scoreText.text = score.ToString ();
+		coinText.text = coins.ToString ();
 	}
-	
-	public void BackButton()
+
+	void SetScoreBasedOnDifficulty ()
 	{
-		SceneManager.LoadScene("MainMenu",LoadSceneMode.Single);
+		if (GamePreferences.GetEasyDifficultyState () == 1)
+		{
+			SetScore (GamePreferences.GetEasyDifficultyHighScore (), GamePreferences.GetEasyDifficultyCoinScore ());
+		}
+
+		if (GamePreferences.GetMediumDifficultyState () == 1)
+		{
+			SetScore (GamePreferences.GetMediumDifficultyHighScore (), GamePreferences.GetMediumDifficultyCoinScore ());
+		}
+
+		if (GamePreferences.GetHardDifficultyState () == 1)
+		{
+			SetScore (GamePreferences.GetHardDifficultyHighScore (), GamePreferences.GetHardDifficultyCoinScore ());
+		}
+
+	}
+
+	public void BackButton ()
+	{
+		SceneManager.LoadScene ("MainMenu", LoadSceneMode.Single);
 	}
 
 }
