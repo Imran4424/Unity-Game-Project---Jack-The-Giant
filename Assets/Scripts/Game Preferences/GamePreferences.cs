738c0f8bcb2ec6aba7e8b65ab6491f8d5e744944
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-25 09:42:36 +0600

    jack the giant - game preferences

--- Assets/Scripts/Game Preferences/GamePreferences.cs
+++ Assets/Scripts/Game Preferences/GamePreferences.cs
@@ -9,16 +9,13 @@ public static class GamePreferences
 	public static string MediumDifficulty = "MediumDifficulty";
 	public static string HardDifficulty = "HardDifficulty";
 
-
 	public static string EasyDifficultyScore = "EasyDifficultyScore";
 	public static string MediumDifficultyScore = "MediumDifficultyScore";
 	public static string HardDifficultyScore = "HardDifficultyScore";
 
-
 	public static string EasyDifficultyCoinScore = "EasyDifficultyCoinScore";
 	public static string MediumDifficultyCoinScore = "MediumDifficultyCoinScore";
 	public static string HardDifficultyCoinScore = "HardDifficultyCoinScore";
-
 
 	public static string IsMusicOn = "IsMusicOn";
 
@@ -27,14 +24,19 @@ public static class GamePreferences
 	0 - false, 1 - true
 	*/
 
-	public static int GetMusicState()
+	public static int GetMusicState ()
 	{
-		return PlayerPrefs.GetInt(GamePreferences.IsMusicOn);
+		return PlayerPrefs.GetInt (GamePreferences.IsMusicOn);
 	}
 
-	public static void SetMusicState(int state)
+	public static void SetMusicState (int state)
 	{
-		PlayerPrefs.SetInt(GamePreferences.IsMusicOn,state);
+		PlayerPrefs.SetInt (GamePreferences.IsMusicOn, state);
+	}
+
+	public static int GetEasyDifficultyState()
+	{
+		return PlayerPrefs.GetInt(GamePreferences.EasyDifficulty);
 	}
 
 }
