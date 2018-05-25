0170bb63760c3ee72cd88a50c36df1466c60c660
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-25 15:58:07 +0600

    jack the giant - game preferences

--- Assets/Scripts/Game Controllers/OptionsController.cs
+++ Assets/Scripts/Game Controllers/OptionsController.cs
@@ -3,28 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class OptionsController : MonoBehaviour {
+public class OptionsController : MonoBehaviour
+{
 
 	[SerializeField]
 	private GameObject easySign, mediumSign, HardSign;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+
 	}
 
-	void SetInitialDifficulty()
+	void SetInitialDifficulty (string difficulty)
 	{
-		
+		switch (difficulty)
+		{
+			case "easy":
+				easySign.SetActive (true);
+				mediumSign.SetActive (false);
+				HardSign.SetActive (false);
+				break;
+
+			case "medium":
+				easySign.SetActive (false);
+				mediumSign.SetActive (true);
+				HardSign.SetActive (false);
+				break;
+
+			case "hard":
+				easySign.SetActive (false);
+				mediumSign.SetActive (false);
+				HardSign.SetActive (true);
+				break;
+
+		}
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+
 	}
 
-	public void BackButton()
+	public void BackButton ()
 	{
-		SceneManager.LoadScene("MainMenu",LoadSceneMode.Single);
+		SceneManager.LoadScene ("MainMenu", LoadSceneMode.Single);
 	}
 }
