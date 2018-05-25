1a01f16d6b3d0a778cd574c4eeebcb9ffe91cbc8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-25 22:43:38 +0600

    jack the giant - game preferences

--- Assets/Scripts/Game Controllers/GameManager.cs
+++ Assets/Scripts/Game Controllers/GameManager.cs
@@ -108,6 +108,8 @@ public class GameManager : MonoBehaviour
 			{
 				int highScore = GamePreferences.GetEasyDifficultyHighScore();
 				int coinHighScore = GamePreferences.GetEasyDifficultyCoinScore();
+
+				
 			}
 
 			gameStartedFormMainMenu = false;
