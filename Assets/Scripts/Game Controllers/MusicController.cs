13891e8affa86948542334be2990f8fd5a24c06f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-26 09:12:51 +0600

    jack the giant - game preferences

--- Assets/Scripts/Game Controllers/MusicController.cs
+++ Assets/Scripts/Game Controllers/MusicController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class MusicController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	void Awake()
+	{
 		
 	}
+	
 	
 	// Update is called once per frame
 	void Update () {
