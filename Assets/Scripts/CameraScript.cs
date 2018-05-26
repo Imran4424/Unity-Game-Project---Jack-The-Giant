b66d4d94ff93344c9e1c6d6b89bc07c20b3b1b2a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-05-26 08:48:19 +0600

    jack the giant - game preferences

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -2,52 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraScript : MonoBehaviour {
-
+public class CameraScript : MonoBehaviour
+{
 
 	private float speed = 1f;
 	private float acceleration = 0.1f;
 	private float maxSpeed = 3.2f;
 
-
 	private float easySpeed = 3.2f;
 	private float mediumSpeed = 3.7f;
 	private float hardSpeed = 4.2f;
-
-
-
 
 	[HideInInspector]
 	public bool movecamera;
 
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
-		if (GamePreferences.GetEasyDifficultyState() == 1)
+		if (GamePreferences.GetEasyDifficultyState () == 1)
+		{
+			maxSpeed = easySpeed;
+		}
+
+		if (GamePreferences.GetMediumDifficultyState() == 1)
+		{
+			maxSpeed = mediumSpeed;
+		}
+
+		if (GamePreferences.GetEasyDifficultyState () == 1)
 		{
 			maxSpeed = easySpeed;
 		}
 
 		movecamera = true;
 	}
-	
+
 	// Update is called once per frame
-	void Update () 
+	void Update ()
 	{
-		if(movecamera)
+		if (movecamera)
 		{
-			moveCamera();
+			moveCamera ();
 		}
 	}
 
-	void moveCamera()
+	void moveCamera ()
 	{
 		Vector3 temp = transform.position;
 
 		float oldY = temp.y;
 		float newY = temp.y - (speed * Time.deltaTime);
 
-		temp.y = Mathf.Clamp(temp.y,oldY,newY);
+		temp.y = Mathf.Clamp (temp.y, oldY, newY);
 
 		transform.position = temp;
 
